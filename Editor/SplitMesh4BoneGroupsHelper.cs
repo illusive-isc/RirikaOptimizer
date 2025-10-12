8e04bcf0799b265d23f60cd8a3136094f72ba60b
illusice_isc <sheryl.fire.1995@gmail.com>
2025-10-12 10:17:31 +0900

    v1.3.1 前提ツールの修正によるぬいぐるみ召喚の調整

--- Editor/SplitMesh4BoneGroupsHelper.cs
+++ Editor/SplitMesh4BoneGroupsHelper.cs
@@ -3,6 +3,8 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace jp.illusive_isc
 {
@@ -11,45 +13,70 @@ namespace jp.illusive_isc
         [MenuItem("GameObject/illusive_tools/ぬいぐるみ召喚 -IllRirikaOptimizer-", false, 0)]
         private static void SplitSelectedSMR(MenuCommand command)
         {
-            GameObject selected = Selection.activeGameObject;
-
-            GameObject prefab = LoadGameObjectFromGuid("ad44ac2e3d055034da3e6760294c8bea");
-            if (prefab == null)
-                return;
-            SkinnedMeshRenderer smr = prefab
-                .transform.Find("Bag")
-                .GetComponent<SkinnedMeshRenderer>();
-
-            SplitMesh4BoneGroups splitter = ScriptableObject.CreateInstance<SplitMesh4BoneGroups>();
-
-            string nuigurumiPath =
-                "Armature/Hips/Spine/Chest/Z_Bag_root/bag/bag.001/bag.002/bag_nuigurumi";
-            var nuigurumiTransform = prefab.transform.Find(nuigurumiPath).transform;
-            var nuigurumiTransform1 = nuigurumiTransform.Find("bag_nuigurumi.001").transform;
-            var nuigurumiTransform2 = nuigurumiTransform1.Find("bag_nuigurumi.002").transform;
-            var nuigurumiTransform3 = nuigurumiTransform2.Find("bag_nuigurumi.003").transform;
-            var groups = new List<SplitMesh4BoneGroups.BoneGroup>
+            var nui = AssetDatabase.LoadAssetAtPath<GameObject>(
+                "Assets/RirikaOptimizer/GeneratedMeshes/ぬいぐるみ.prefab"
+            );
+            GameObject container;
+            if (nui != null)
             {
-                new()
+                container = PrefabUtility.InstantiatePrefab(nui) as GameObject;
+                if (container != null)
                 {
-                    name = "金具",
-                    bones = new() { nuigurumiTransform },
-                },
-                new()
+                    container.name = nui.name;
+                }
+            }
+            else
+            {
+                GameObject prefab = LoadGameObjectFromGuid("ad44ac2e3d055034da3e6760294c8bea");
+                if (prefab == null)
+                    return;
+                SkinnedMeshRenderer smr = prefab
+                    .transform.Find("Bag")
+                    .GetComponent<SkinnedMeshRenderer>();
+
+                SplitMesh4BoneGroups splitter =
+                    ScriptableObject.CreateInstance<SplitMesh4BoneGroups>();
+
+                string nuigurumiPath =
+                    "Armature/Hips/Spine/Chest/Z_Bag_root/bag/bag.001/bag.002/bag_nuigurumi";
+                var nuigurumiTransform = prefab.transform.Find(nuigurumiPath).transform;
+                var nuigurumiTransform1 = nuigurumiTransform.Find("bag_nuigurumi.001").transform;
+                var nuigurumiTransform2 = nuigurumiTransform1.Find("bag_nuigurumi.002").transform;
+                var nuigurumiTransform3 = nuigurumiTransform2.Find("bag_nuigurumi.003").transform;
+                var groups = new List<SplitMesh4BoneGroups.BoneGroup>
                 {
-                    name = "ぬいぐるみ",
-                    bones = new() { nuigurumiTransform1, nuigurumiTransform2, nuigurumiTransform3 },
-                },
-            };
-            GameObject container = splitter.SplitAllGroups("ぬいぐるみ", smr, groups);
+                    new()
+                    {
+                        name = "金具",
+                        bones = new() { nuigurumiTransform },
+                    },
+                    new()
+                    {
+                        name = "ぬいぐるみ",
+                        bones = new()
+                        {
+                            nuigurumiTransform1,
+                            nuigurumiTransform2,
+                            nuigurumiTransform3,
+                        },
+                    },
+                };
+                container = splitter.SplitAllGroups(
+                    "ぬいぐるみ",
+                    smr,
+                    groups,
+                    null,
+                    "RirikaOptimizer"
+                );
+            }
 
             if (container != null)
             {
+                GameObject selected = Selection.activeGameObject;
                 container.transform.GetChild(0).rotation = Quaternion.Euler(0, 0, 174);
                 container.transform.SetParent(selected.transform, false);
                 Undo.RegisterCreatedObjectUndo(container, "Split Mesh 4 Bone Groups");
                 Selection.activeGameObject = container;
-
             }
         }
 
